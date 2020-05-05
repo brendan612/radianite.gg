f9cc0399daee783fb49361de93bb23236a211126
Brendan <brendan612@gmail.com>
2020-05-05 17:15:53 -0500

    ran first build

---
+++ build/precache-manifest.0d69b9102285a5cd020269324a2ece2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2787157ca62dd84f980dbc83f884391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32b3aae3e67fe480073f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dfff929f1fc27519e79f",
+    "url": "/static/js/2.8148afb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8148afb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b3aae3e67fe480073f",
+    "url": "/static/js/main.2bb5a5c4.chunk.js"
+  },
+  {
+    "revision": "bd6b8c3b632d9b61ea25",
+    "url": "/static/js/runtime-main.851f8d75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
